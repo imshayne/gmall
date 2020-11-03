f9840119fcd1183900b75afce49fbd53008e55b9
Santino Yanz <santino_yan@outlook.com>
2020-11-03 17:10:25 +0800

    - 项目初始化 - 配置各个模块

---
+++ gmall-coupon/src/main/java/work/shyan/gmall/coupon/GmallCouponApplication.java
@@ -0,0 +1,13 @@
+package work.shyan.gmall.coupon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GmallCouponApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GmallCouponApplication.class, args);
+	}
+
+}
---
+++ gmall-coupon/src/test/java/work/shyan/gmall/coupon/GmallCouponApplicationTests.java
@@ -0,0 +1,13 @@
+package work.shyan.gmall.coupon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GmallCouponApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
